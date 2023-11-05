ec0d16521a28636d380e0673e87be251c7c12336
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2023-11-04 22:51:36 -0300

    landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,103 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Navbar from './components/Navbar'
+import FooterNav from './components/FooterNav'
+// import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  // const [data, setData] = useState([])
+
+  // useEffect(() => {
+    
+  // }, [])
+  
   return (
+    <>
+
+    <Navbar></Navbar>
+    
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center p-24 bg-blue-500 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+      <h1 className='text-4xl hover:text-yellow-400 mb-20'>Gosta de competir? Você veio para o lugar certo!</h1>
+
+      <img src="https://rotaperdidablog.files.wordpress.com/2016/05/celular-x-relogio.png" alt="pessoas olhando no celular" className='mb-10 w-full' />
+
+      <h2 className='font-bold text-xl'>Criar uma competição é fácil!</h2>
+
+      <div className='w-screen h-80 p-4 flex flex-row my-3 justify-around'>
+        <div className='flex flex-col justify-around'>
+          <h2 className='text-2xl font-medium'>Passo 1:</h2>
+          <p className='pl-5 text-3xl'>Escreva o nome da sua competição.</p>
+        </div>
+        <img src="https://pbs.twimg.com/media/EmyURxlXMAQSmHD.png" alt="nome" className='w-fit h-full' />
+      </div>
+      
+      {/* https://static.vecteezy.com/ti/vetor-gratis/p1/2919386-time-of-e-sports-atleta-competicao-em-competicao-de-esporte-vetor.jpg */}
+      <div className='w-screen h-80 p-4 flex flex-row my-3 justify-around'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLt9x61RpLkSYmqC-GQgFrPR2d193x4UV-Q&usqp=CAU" alt="categoria" className='w-fit h-full' />
+        <div className='flex flex-col justify-around'>
+          <h2 className='text-2xl font-medium'>Passo 2:</h2>
+          <p className='pr-5 text-3xl'>Escolha a categoria da sua competição.</p>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='w-screen h-80 p-4 flex flex-row my-3 justify-around'>
+        <div className='flex flex-col justify-around'>
+          <h2 className='text-2xl font-medium'>Passo 3:</h2>
+          <p className='pl-5 text-3xl'>Defina um preço para entrar (ou 0, se for grátis :O).</p>
+        </div>
+        <img src="https://www.supplymidia.com.br/wp-content/uploads/2020/09/searching-magnifier-glass-dollar-dollar-icon-under-magnifying-glass-picture-id1180233758-724x480.jpg" alt="preço" className='w-fit h-full' />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='w-screen h-80 p-4 flex flex-row my-3 justify-around'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr4g25PGJjN26sGNlR6X2Rye1gwfkK9ViMKA&usqp=CAU" alt="premio" className='w-fit h-full' />
+        <div className='flex flex-col justify-around'>
+          <h2 className='text-2xl font-medium'>Passo 4:</h2>
+          <p className='pr-5 text-3xl'>Defina um prêmio (ou 0, se não tiver prêmio).</p>
+        </div>
       </div>
+
+      <div className='w-screen h-80 p-4 flex flex-row my-3 justify-around'>
+        <div className='flex flex-col justify-around'>
+          <h2 className='text-2xl font-medium'>Passo 5:</h2>
+          <p className='pl-5 text-3xl'>Defina quem receberá o prêmio.</p>
+        </div>
+        <img src="https://emvisao.com/wp-content/uploads/2019/11/competi%C3%A7%C3%A3o-capa.png" alt="ganhadores" className='w-fit h-full' />
+      </div>
+
+      <h2 className='my-4 underline'>Crie uma conta agora para começar a competir!</h2>
+      <form action="" className='flex flex-col m-3 items-center w-full'>
+
+        <div className='flex flex-col'>
+          <div className='flex flex-row h-10'>
+            <div className='flex flex-row'>
+              <label htmlFor="nome">Nome:</label>
+              <input type="text" name="nome" className='text-black mx-5' />
+            </div>
+            <div className='flex flex-row'>
+              <label htmlFor="preco">Senha:</label>
+              <input type="password" name="senha" className='text-black mx-5'  />
+            </div>
+          </div>
+
+          <div className='flex flex-row m-3'>
+            <label htmlFor="premio">Email:</label>
+            <input type="email" name="email" className='text-black mx-5 w-full'  />
+          </div>
+        </div>
+
+        <button type="submit" className='border border-gray-50 m-5 p-5 bg-slate-400 hover:bg-slate-500'>Criar conta!</button>
+      </form>
+
+      <button className='text-xl border border-black p-3 my-20 bg-slate-400 hover:bg-slate-500'>Ou veja todas as competições já criadas</button>
+      <img className='w-full' src="https://media.istockphoto.com/id/89285817/pt/foto/pessoas-de-neg%C3%B3cios-a-caminho.jpg?s=612x612&w=0&k=20&c=6ozHiepq1d-E16Tkek5DJjTqXJULuBZM0GVLomDcu4M=" alt="" />
     </main>
+
+    <FooterNav></FooterNav>
+
+    </>
   )
 }
